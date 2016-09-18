b1631d4adad8934ede33cfa4bd2dfcc994db5c89
Patrick Gaffney <patrickrgaffney@patmac.local>
2016-09-17 20:53:14 -0500

    ADD: dotfiles.sh installation script

---
+++ dotfiles.sh
@@ -0,0 +1,30 @@
+#####
+# dotfiles.sh -- install dotfiles symlinks
+#
+#   @author     Pat Gaffney <pat@hypepat.com>
+#   @created    2016-09-17
+#   @modified   2016-09-17
+#
+#   This file creates symlinks for the dotfiles in the *current
+#   directory* to $HOME.
+#
+#####################################################################
+
+# Symlink all the dotfiles.
+# ln -s :: create symbolic link
+# ln -v :: verbose mode- print details
+
+regex='([\.]\w*)'
+
+# Create a symlink for each file in this directory.
+for file in $(ls -A)
+do
+    if [[ -d $file ]]; then 
+        printf "dotfiles.sh: Skipping \'%s\' -- it is a directory\n" $file
+        continue;
+    elif [[ -x $file ]]; then
+        printf "dotfiles.sh: Skipping \'%s\' -- it is an executable\n" $file
+        continue;
+    else ln -sv "$PWD/$file" "$HOME";
+    fi
+done
