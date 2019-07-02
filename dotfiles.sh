1ad4f1d0be95f608bc52490006aa5de1e654aa44
Pat Gaffney <pat@hypepat.com>
2019-07-02 12:32:29 -0500

    Updates to dotfiles.sh from shellcheck

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,9 +1,10 @@
+#!/usr/bin/env bash
+
 #####
 # dotfiles.sh -- install dotfiles symlinks
 #
 #   author:     Pat Gaffney <pat@hypepat.com>
 #   created:    2016-09-17
-#   modified:   2017-04-10
 #
 #   This file creates symlinks for the dotfiles in the *current
 #   directory* to $HOME.
@@ -18,24 +19,23 @@ echo $'\e[1;34mInstalling symlinks:\e[0m'
 
 # Create a symlink for each appropriate file
 # in this directory to $HOME.
-for file in $(ls -A)
-do
+for file in *; do
     if [[ -d $file ]]; then 
-        printf "dotfiles.sh: Skipping \'%s\' -- it is a directory\n" $file
+        printf "dotfiles.sh: Skipping \'%s\' -- it is a directory\n" "$file"
     elif [[ -x $file ]]; then
-        printf "dotfiles.sh: Skipping \'%s\' -- it is an executable\n" $file
+        printf "dotfiles.sh: Skipping \'%s\' -- it is an executable\n" "$file"
     elif [[ $file == *.png ]]; then
-        printf "dotfiles.sh: Skipping \'%s\' -- it is a picture\n" $file
+        printf "dotfiles.sh: Skipping \'%s\' -- it is a picture\n" "$file"
     elif [[ $file == .gitignore ]]; then
-        printf "dotfiles.sh: Skipping \'%s\' -- it is a gitignore\n" $file
+        printf "dotfiles.sh: Skipping \'%s\' -- it is a gitignore\n" "$file"
     elif [[ $file == *.md ]]; then
-        printf "dotfiles.sh: Skipping \'%s\' -- it is a Markdown file\n" $file
+        printf "dotfiles.sh: Skipping \'%s\' -- it is a Markdown file\n" "$file"
     elif [[ $file == *.terminal ]]; then
-        printf "dotfiles.sh: Skipping \'%s\' -- it is a Terminal settings file\n" $file
+        printf "dotfiles.sh: Skipping \'%s\' -- it is a Terminal settings file\n" "$file"
     elif [[ $file == *.itermcolors ]]; then
-        printf "dotfiles.sh: Skipping \'%s\' -- it is an iTerm2 color file\n" $file
+        printf "dotfiles.sh: Skipping \'%s\' -- it is an iTerm2 color file\n" "$file"
     elif [[ $file == *.json ]]; then
-        printf "dotfiles.sh: Skipping \'%s\' -- it is a JSON file\n" $file
+        printf "dotfiles.sh: Skipping \'%s\' -- it is a JSON file\n" "$file"
     else ln -sv "$PWD/$file" "$HOME";
     fi
 done
